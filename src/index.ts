8a11190a963f511872c650b722a20172035ced45
Winrey <i@iaside.com>
2021-11-12 11:04:59 +0800

    chore: update import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import locker from './locker';
 import code from './code';
-import wait from './wait';
+import { wait as waitFunc } from './wait';
 import error from './error';
 
-export default { ...locker, ...code, ...wait, ...error };
+export const lock = code.lock
+export const Locker = locker.Locker
+export const wait = waitFunc
+export const LockerError = error.LockerError
+export const WrongTicketError = error.WrongTicketError
+export const TicketUnvalidError = error.TicketUnvalidError
+export const LockerClearError = error.LockerClearError
+export const LockerTimeoutError = error.LockerTimeoutError
+
+export default { ...locker, ...code, wait, ...error };
